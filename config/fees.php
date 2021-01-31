adb6cd75bf38ca41409921b4897be6b495dfb4b3
Jacob Hyde <jacob@jacobhyde.com>
2021-01-31 15:13:29 -0800

    Adds comments to config file

--- config/fees.php
+++ config/fees.php
@@ -3,6 +3,10 @@
 return [
     //Users Model
     'user_model' => \App\Models\User::class,
+
+    //Location model
     'location_model' => \App\Models\Location::class,
+
+    //Booking Model
     'booking_class' => \App\Models\Booking::class,
 ];
