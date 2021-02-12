febe9cea13e894fd6ebebdb10c3a5baee17b2898
auxiliawebsitedesign <auxiliawebsitedesign@users.noreply.github.com>
2021-02-12 19:00:28 +0000

    Fix styling

--- src/FeesServiceProvider.php
+++ src/FeesServiceProvider.php
@@ -7,8 +7,6 @@ use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Tipoff\Fees\Models\Fee;
 use Tipoff\Fees\Policies\FeePolicy;
-use Tipoff\Support\TipoffPackage;
-use Tipoff\Support\TipoffServiceProvider;
 
 class FeesServiceProvider extends PackageServiceProvider
 {
