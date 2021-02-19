c6a11786054ef720039988c45c2de1bb453a3afa
Phuc Le <phuclh93@gmail.com>
2021-02-19 10:32:48 -0800

    [TGER-71] Add HasUpdater to model.

--- src/Models/LocationFee.php
+++ src/Models/LocationFee.php
@@ -8,10 +8,12 @@ use Tipoff\Fees\Database\Factories\FeeFactory;
 use Tipoff\Support\Models\BaseModel;
 use Tipoff\Support\Traits\HasCreator;
 use Tipoff\Support\Traits\HasPackageFactory;
+use Tipoff\Support\Traits\HasUpdater;
 
 class LocationFee extends BaseModel
 {
     use HasCreator;
+    use HasUpdater;
     use HasPackageFactory;
 
     protected static function newFactory()
